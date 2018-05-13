948ff64e4d14145f1873a64399d17fb00adc3e8e
Sergei <sergei.visotsky@gmail.com>
2018-05-13 11:32:41 +0300

    More functionality added

---
+++ LinkedList/LinkedList/DoublyLinkedList.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    interface IList
+    {
+        bool IsEmpty();
+
+        bool IsFull();
+
+        int GetSize();
+
+        void InsertFirst(int element);
+
+        void InsertAtPosition(int element, int position);
+
+        void InsertLast(int element);
+
+        void Display();
+
+        void DeleteAtPosition(int position);
+
+        int NegativeElementQuantity();
+    }
+
+    class Node
+    {
+        public int Data;
+        public Node Prev;
+        public Node Next;
+
+        public Node(int data)
+        {
+            Data = data;
+        }
+    }
+
+    public class DoublyLinkedList : IList
+    {
+        private int Size = 0;
+        private int MaxSize;
+        private Node Head = null;
+        private Node Tail = null;
+
+
+        public DoublyLinkedList(int maxSize)
+        {
+            MaxSize = maxSize;
+        }
+
+        public void Display()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Linked list is empty!");
+            }
+
+            if ((Head.Next == null))
+            {
+                Console.WriteLine(Head.Data);
+                return;
+            }
+
+            Node node;
+            Console.WriteLine(Head.Data + "\t");
+            node = Head.Next;
+
+            while (node.Next != null)
+            {
+                Console.WriteLine(node.Data + "\t");
+                node = node.Next;
+            }
+
+            Console.WriteLine(node.Data + "\t");
+        }
+
+        public int GetSize()
+        {
+            return Size;
+        }
+
+        public void InsertAtPosition(int element, int position)
+        {
+            Node node = new Node(element);
+            if (position == 1)
+            {
+                InsertFirst(element);
+                return;
+            }
+
+            Node ptr = Head;
+
+            for (int i = 2; i <= Size; i++)
+            {
+                if (i == position)
+                {
+                    Node temp = ptr.Next;
+                    ptr.Next = node;
+                    node.Prev = ptr;
+                    node.Next = temp;
+                    temp.Prev = node;
+                }
+
+                ptr = ptr.Next;
+            }
+
+            Size++;
+        }
+
+        public void InsertFirst(int element)
+        {
+            Node node = new Node(element);
+            if (IsEmpty())
+            {
+                Head = node;
+                Tail = Head;
+            }
+            else
+            {
+                Head.Prev = node;
+                node.Next = Head;
+                Head = node;
+            }
+            Size++;
+        }
+
+        public void InsertLast(int element)
+        {
+            Node node = new Node(element);
+            if (Head == null)
+            {
+                Head = node;
+                Tail = Head;
+            }
+            else
+            {
+                node.Prev = Tail;
+                Tail.Next = node;
+                Tail = node;
+            }
+
+            Size++;
+        }
+
+        public bool IsEmpty()
+        {
+            return Head == null;
+        }
+
+        public bool IsFull()
+        {
+            return Size == MaxSize;
+        }
+
+        public void DeleteAtPosition(int position)
+        {
+            if (position == 1)
+            {
+                if (Size == 1)
+                {
+                    Head = null;
+                    Tail = null;
+                    Size = 0;
+                    return;
+                }
+
+                Head = Head.Next;
+                Head.Prev = null;
+                Size--;
+                return;
+            }
+
+            if (position == Size)
+            {
+                Tail = Tail.Prev;
+                Tail.Prev = null;
+                Size--;
+            }
+            Node node = Head.Next;
+            for (int i = 1; i <= Size; i++)
+            {
+                if (i == position)
+                {
+                    Node prevPtr = node.Prev;
+                    Node nextPtr = node.Next;
+                    prevPtr.Next = nextPtr;
+                    nextPtr.Prev = prevPtr;
+                    Size--;
+                    return;
+                }
+                node = node.Next;
+            }
+        }
+
+        public int NegativeElementQuantity()
+        {
+            Node node;
+            int counter = 0;
+            for (node = Head; node != Tail; node = node.Next)
+            {
+                if (node.Data < 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        static void Main(string[] args)
+        {
+            int answer;
+            String yesNoAnswer;
+            int element;
+            int position;
+
+            Console.Write("Input size: (max 15)");
+            int maxSize = Console.Read();
+            if(maxSize > 15)
+            {
+                Console.WriteLine("List size shouldn't be greater than 15!");
+            }
+            DoublyLinkedList list = new DoublyLinkedList(maxSize);
+        }
+    }
+}
---
+++ LinkedList/LinkedListTests/DoublyLinkedListTests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList.Tests
+{
+    [TestClass()]
+    public class LinkedListTests
+    {
+        DoublyLinkedList List = new DoublyLinkedList(15);
+        private readonly int Iterator = 15;
+        private Random rd = new Random();
+
+        [TestMethod()]
+        public void EmptyStatusTest()
+        {
+            Assert.IsTrue(List.IsEmpty());
+            Assert.AreEqual(0, List.GetSize());
+        }
+
+        [TestMethod()]
+        public void InsertFirstTest()
+        {
+            for (int i = 0; i < Iterator; i++)
+            {
+                List.InsertFirst(rd.Next(99) + 1);
+            }
+            Assert.IsTrue(!List.IsEmpty());
+            Assert.AreEqual(15, List.GetSize());
+        }
+
+        [TestMethod()]
+        public void InsertAtPositionTest()
+        {
+            List.InsertFirst(45);
+            List.InsertAtPosition(5, 1);
+            List.Display();
+            Assert.IsTrue(!List.IsEmpty());
+            Assert.AreEqual(2, List.GetSize());
+        }
+
+        [TestMethod()]
+        public void InsertLastTest()
+        {
+            for(int i = 0; i < Iterator - 1; i++)
+            {
+                List.InsertFirst(rd.Next(99) + 1);
+            }
+            List.InsertLast(76);
+            Assert.IsTrue(!List.IsEmpty());
+            Assert.AreEqual(15, List.GetSize());
+        }
+
+        [TestMethod()]
+        public void NegativeElementQuantityTest()
+        {
+            List.InsertFirst(5);
+            for(int i = 0; i <= 3; i++)
+            {
+                List.InsertAtPosition(-90, i);
+            }
+            List.InsertLast(7);
+            Assert.AreEqual(3, List.NegativeElementQuantity());
+        }
+
+        [TestMethod()]
+        public void DeleteAtPositionTest()
+        {
+            for(int i = 0; i <= Iterator; i++)
+            {
+                List.InsertAtPosition(i, i);
+            }
+            Assert.IsTrue(List.IsFull());
+            Assert.AreEqual(15, List.GetSize());
+            for(int i = 0; i <= Iterator; i++)
+            {
+                List.DeleteAtPosition(i);
+            }
+            Assert.IsTrue(List.IsEmpty());
+            Assert.AreEqual(0, List.GetSize());
+        }
+    }
+}
